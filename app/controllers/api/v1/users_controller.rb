ad760fa0d7719442a25fa81bb486d8912a7c83e7
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-28 23:39:17 +0200

    Fix linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,43 +1,39 @@
 class Api::V1::UsersController < ApplicationController
-  class UsersController < ApplicationController
-      
-      # GET /api/v1/users
-      def index
-        users = User.all
-        render json: users
-      end
-  
-      # GET /api/v1/users/:id
-      def show
-        user = User.find(params[:id])
-        render json: user
-      end
+  # GET /api/v1/users
+  def index
+    users = User.all
+    render json: users
+  end
 
-      # POST /api/v1/users
-      def create
-        user = User.new(user_params)
-        if user.save
-          render json: user, status: :created
-        else
-          render json: user.errors, status: :unprocessable_entity
-        end
-      end
+  # GET /api/v1/users/:id
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
 
-      # DELETE /api/v1/users/:id
-      def update
-        user = User.find(params[:id])
-        if user.update(user_params)
-          render json: user
-        else
-          render json: user.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+  # POST /api/v1/users
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /api/v1/users/:id
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
