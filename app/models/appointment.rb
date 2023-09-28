f61dbc58b772facf7baee3716b097b978999c40e
forLoop94 <charlesuwem94@gmail.com>
2023-09-28 15:35:13 +0100

    Add specialization model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ApplicationRecord
+  belongs_to :user
+  belongs_to :doctor
+
+  validates :appointment_date, :appointment_time, :duration, :user_id, :doctor_id, presence: true
 end
