96dcc486fef4526f3eaa1710aa61e9998ec80b08
forLoop94 <charlesuwem94@gmail.com>
2023-09-28 15:05:32 +0100

    Generate doctor and appointment model, include associations and validations

---
+++ app/models/doctor.rb
@@ -0,0 +1,4 @@
+class Doctor < ApplicationRecord
+  belongs_to :specialization
+  validates :name, :bio, :fee_per_appointment, :specialization_id, :bio, :time_available_from, :time_available_to, presence: true
+end
