1284e1b7edebecd18e97d2358519696d8e9c747a
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-29 10:50:00 +0200

    Add routes for doctors and appointments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
-      resources
-      resources 
+      resources :doctors, only: [:index, :show, :new, :create, :destroy] do
+        resources :appointments, only: [:index, :show, :new, :create, :destroy]
+      end 
     end
   end
 end
