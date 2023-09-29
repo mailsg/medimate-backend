600ce147978d52d3b646f093361553469be680fd
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-29 10:39:38 +0200

    Make changes in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources
+      resources 
+    end
+  end
 end
