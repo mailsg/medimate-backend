eee80dbde0eafdc989ad254caa89bd43da4d01fe
forLoop94 <charlesuwem94@gmail.com>
2023-09-30 09:43:44 +0100

    Add show and create actions for doctors. Make sure they can be used in the front end

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -3,4 +3,24 @@ class Api::V1::DoctorsController < ApplicationController
     current_user
     render json: Doctor.all
   end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+    render json: @doctor
+  end
+
+  def create
+    @user = current_user
+    @doctor = user.doctors.build(doctor_params)
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: { errors: @doctor.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :time_available_from, :time_available_to, :bio, :fee_per_appointment, :specialization_id, :image, :user_id)
+  end
+
 end
