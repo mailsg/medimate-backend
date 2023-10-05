3d3130b9cb5236bfccbc30f9869b23c2d7d1b1c1
Sandeep Ghosh <mailsg0912@gmail.com>
2023-10-05 12:06:15 +0530

    Check for linter errors

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Doctor, type: :model do
   describe 'validation tests' do
     subject do
-      @user1 = User.new(username: 'Lorem Ipsum', email: 'lorem@ipsum.com', password: '123456', password_confirmation: '123456')
+      @user1 = User.new(username: 'Lorem Ipsum', email: 'lorem@ipsum.com', password: '123456',
+                        password_confirmation: '123456')
       Doctor.new(name: 'Dr. Dolittle', location: 'USA', specialization_id: 10, fee_per_appointment: 400,
-                bio: '10 Years of experience', time_available_from: '08:00:00', time_available_to: '20:00:00')
+                 bio: '10 Years of experience', time_available_from: '08:00:00', time_available_to: '20:00:00')
     end
     before { subject.save }
 
--- spec/routes/appointments_route_spec.rb
+++ spec/routes/appointments_route_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe Api::V1::AppointmentsController, type: :routing do
   describe 'route' do
     it 'routes to #index' do
       expect(get: '/api/v1/appointments').to route_to({ 'format' => 'json', 'controller' => 'api/v1/appointments',
-                                                    'action' => 'index' })
+                                                        'action' => 'index' })
     end
 
     it 'routes to #create' do
       expect(post: 'api/v1/appointments').to route_to({ 'format' => 'json', 'controller' => 'api/v1/appointments',
-                                                    'action' => 'create' })
+                                                        'action' => 'create' })
     end
 
     it 'routes to #destroy' do
       expect(delete: 'api/v1/appointments/1').to route_to({ 'format' => 'json', 'controller' => 'api/v1/appointments',
-                                                        'action' => 'destroy', 'id' => '1' })
+                                                            'action' => 'destroy', 'id' => '1' })
     end
   end
 end
