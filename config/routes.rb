cd6a9b72a3041efee14e0977164678c321e6f799
Sandeep Ghosh <mailsg0912@gmail.com>
2023-10-08 10:09:38 +0530

    Add specializations controller

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: 'json' } do
       resources :doctors, only: [:index, :show, :new, :create, :destroy] 
       resources :appointments, only: [:index, :show, :new, :create, :destroy]
+      resources :specializations, only: [:index, :create]
     end
   end
 end
